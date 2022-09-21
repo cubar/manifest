2600b9799fe54e174e6164c1feee2dadba8f71ec
Sreini <36940685+Sreini@users.noreply.github.com>
2022-09-21 10:02:29 +0200

    Update wsgi.py
    changed back wsgi

--- manifest/wsgi.py
+++ manifest/wsgi.py
@@ -8,17 +8,10 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
 
 import os
-import environ
 
 from django.core.wsgi import get_wsgi_application
 
-PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-BASE_DIR = os.path.dirname(PROJECT_DIR)
-
-env = environ.Env()
-environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "manifest.settings.%s" % (env('ENV_VALUE')) )
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "manifest.settings.prod")
 
 application = get_wsgi_application()
 
