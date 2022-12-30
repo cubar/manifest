382fe08b6ff724d9ad0480931d8e314fea670976
Chuck Barkey <chuck@chubar.nl>
2022-12-30 22:21:18 +0100

    Improvements: - no dependency on ssh anymore - performs the table copy for the images - added the table copy for the renditions - both table copies are now performed via a migration - and other minor improvements on the code

--- bin/setup_customimage.py
+++ bin/setup_customimage.py
@@ -1,46 +1,167 @@
 #!/usr/bin/env python
+import re
 import os
 import sys
 from glob import glob
 from environ import Env
-from subprocess import Popen, PIPE, STDOUT, run
+from subprocess import Popen, PIPE
+import psycopg2
+from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
+
+postgres = psycopg2.connect(database='manifestdb')
+postgres.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+
+contents_image_models = """# models.py
+from django.db import models
+from wagtail.images.models import Image, Rendition, AbstractImage, AbstractRendition\n\n
+class CustomImage(AbstractImage):
+    # Add any extra fields to image here
+    {}
+    {}\n
+    admin_form_fields = Image.admin_form_fields + (
+        #Then add the field names here to make them appear in the form:
+        {}
+    )\n\n
+class CustomRendition(AbstractRendition):
+    image = models.ForeignKey(CustomImage, on_delete=models.CASCADE, related_name='renditions')\n
+    class Meta:
+        unique_together = (
+            ('image', 'filter_spec', 'focal_point_key'),
+        )
+"""
+
+sql_setval = '''"select pg_catalog.setval('public.{app}_custom{table}_id_seq', (select max(id) from {app}_custom{table}), true);"'''
+contents_copy_images_migration = '''from django.db import migrations\n
+{copy_image}{copy_rendition}
+class Migration(migrations.Migration):
+    dependencies = [
+        ('{app}', '0001_initial'),
+    ]
+    operations = [
+        migrations.RunPython(copy_Image),
+        migrations.RunPython(copy_Rendition),
+        migrations.RunSQL({sql_setval_image}),
+        migrations.RunSQL({sql_setval_redition}),
+    ]
+'''
+copy_table = '''
+def copy_{from_model}(apps, schema_editor):
+    from_model = apps.get_model('wagtailimages', '{from_model}')
+    to_model   = apps.get_model('{app}', '{to_model}')
+    for from_object in from_model.objects.all().order_by('id'):
+        to_object = to_model()
+        for f in from_object._meta.fields:
+            if f.related_model:
+                name = f.get_joining_columns()[0][0]
+            else:
+                name = f.name
+            to_object.__dict__[name] = f.value_from_object(from_object)
+        to_object.save()\n\n'''
 
 
-def my_write(filename, text, show=True):
+def get_contents_images_models_text(complete):
+    """return the text for the model.py file"""
+    def mk_field(field):
+        return f"{field} = models.CharField(max_length=255, blank=True, default='change-this-{field}')"
+
+    if complete:
+        values = [mk_field('source'), mk_field('caption'), "'source', 'caption'"]
+    else:
+        values = ['', '', '']
+    return contents_image_models.format(*values)
+
+
+def makemigration_copy_images():
+    my_cmd(f'./manage.py makemigrations --empty {APP}')
+    migration_0002 = glob(f'{APP}/migrations/0002_*.py')[0]
+    contents = contents_copy_images_migration.format(
+        app=APP,
+        copy_image=copy_table.format(
+            app=APP,
+            from_model='Image',
+            to_model='CustomImage'
+        ),
+        copy_rendition=copy_table.format(
+            app=APP,
+            from_model='Rendition',
+            to_model='CustomRendition'
+        ),
+        sql_setval_image   =sql_setval.format(app=APP, table='image'),     # noqa
+        sql_setval_redition=sql_setval.format(app=APP, table='rendition'),
+    )
+    my_write(migration_0002, contents)
+
+
+def write_customimage_models(complete):
+    contents = get_contents_images_models_text(complete=complete)
+    my_write(f'{APP}/models.py', contents)
+
+
+def my_log(s):
+    print(f'  ======== {s}')
+
+
+def db_exec(sql):
+    rows = []
+    select_query = ('select' == sql.strip()[:6].lower())
+    with postgres.cursor() as cursor:
+        cursor.execute(sql)
+        if select_query:
+            rows = cursor.fetchall()
+    return rows
+
+
+def db_rows(sql):
+    return db_exec(sql)
+
+
+def db_drop_all():
+    rows = db_rows(
+        "select table_name"
+        " from information_schema.tables"
+        " where table_schema='public'"
+    )
+    for row in rows:
+        table = row[0]
+        #print(f'==== dropping table {table}')
+        db_exec(f"drop table {table} cascade")
+
+
+def setval_custom_app_tables():
+    setval('image')
+    setval('rendition')
+
+
+def setval(wat):
+    table = f'{APP}_custom{wat}'
+    rows = db_exec(f"select max(id) from {table}")
+    value = rows[0][0]
+    if not value:
+        return
+    print(f'setval {table=} {value=}')
+    db_exec(f"SELECT pg_catalog.setval('public.{table}_id_seq', {value}, true)")
+
+
+def my_write(filename, text, show=False):
     if show:
         print(f'==========\n{text}\n==========')
     with open(filename, 'w') as fp:
         fp.write(text)
 
 
-def my_run(*args, show=True):
-    process = run(args, capture_output=True)
-    if process.returncode != 0:
-        raise Exception(process.stderr.decode())
-    result = process.stdout.decode().split('\n')
+def my_cmd(cmd, show=False):
     if show:
-        show_sequence(result)
-    return result
-
-def my_cmd(cmd, show=True):
-    if show:
-        print(f'======={cmd=}')
-    process = Popen(cmd, shell=True, stdout=PIPE, stderr=STDOUT)
-    process = process.communicate()[0]
-    result = process.decode().split('\n')
-    if show:
-        show_sequence(result)
-    return result
-
-
-def show_sequence(seq):
-    for line in seq:
-        print(line)
-
-
-def get_migrations():
-    migrations = my_run('./manage.py', 'showmigrations', 'coderedcms')
-    return migrations
+        print(f'{cmd=}')
+    pipe = Popen(cmd, shell=True, stdout=PIPE, stderr=PIPE, universal_newlines=True)
+    #pipe = pipe.communicate()[0]
+    stdout = pipe.stdout.read()
+    stderr = pipe.stderr.read()
+    if stderr:
+        raise Exception(stderr)
+    #result = pipe.decode().split('\n')
+    if show and stdout:
+        print(stdout)
+    return stdout
 
 
 def get_virtualenv():
@@ -72,148 +193,130 @@ def show_mig(mig):
 
 def rm_mig_0036():
     for migration in MIG_0036:
+        print(f'deleting {migration}')
         os.unlink(migration)
 
 
 def reset_settings():
-    my_run(
-        'sed', '-i',
-        '-e', "/^WAGTAILIMAGES_IMAGE_MODEL = 'images.CustomImage'/d",
-        '-e', "/^INSTALLED_APPS.append('images')/d",
-        SETTINGS
-    )
-
-
-def drop_create_db():
-    print(f"About to drop and recreate the database {DATABASE}")
-    my_cmd(f"echo 'drop database {DATABASE}; create database {DATABASE}' | {PSQL_NODB}")
-
-
-def get_images_models_text(app, complete=True):
-    """return the text for the model.py file"""
-    def mk_field(field):
-        return f"{field} = models.CharField(max_length=255, blank=True, default='change-this-string-for-{field}')"
-    if complete:
-        values = [mk_field('source'), mk_field('caption'), "'source', 'caption'"]
-    else:
-        values = ['', '', '']
-
-    # text until:                                            # "==== START-OF-TEXT ===="
-    return f"""# models.py
-from django.db import models
-from wagtail.images.models import Image, AbstractImage, AbstractRendition\n\n
-class CustomImage(AbstractImage):
-    # Add any extra fields to image here
-    {values[0]}
-    {values[1]}\n
-    admin_form_fields = Image.admin_form_fields + (
-        #Then add the field names here to make them appear in the form:
-        {values[2]}
-    )\n\n
-class CustomRendition(AbstractRendition):
-    image = models.ForeignKey(CustomImage, on_delete=models.CASCADE, related_name='renditions')\n
-    class Meta:
-        unique_together = (
-            ('image', 'filter_spec', 'focal_point_key'),
-        )"""                                                 # "==== END-OF-TEXT ===="
-
-
-def get_copy_images_migration():
-    """Here is the text for a migration until"""             # "==== START-OF-TEXT ===="
-    return f'''from django.db import migrations\n\n
-def copy_images(apps, schema_editor):
-    Image = apps.get_model('wagtailimages', 'Image')
-    CustomImage = apps.get_model('{APP}', 'CustomImage')
-    print('')
-    for image in Image.objects.all():
-        print(f'{{image.title=}}')
-        customimage = CustomImage()
-        for f in image._meta.fields:
-            if f.name == 'id':
-                continue
-            customimage.__dict__[f.name] = f.value_from_object(image)
-        customimage.save()\n\n
-class Migration(migrations.Migration):
-    dependencies = [
-        ('{APP}', '0001_initial'),
-    ]
-    operations = [
-        migrations.RunPython(copy_images),
-    ]'''                                                     # "==== END-OF-TEXT ===="
+    with open(SETTINGS) as fp:
+        lines = fp.readlines()
+    with open(SETTINGS, 'w') as fp:
+        for line in lines:
+            if line not in [
+                "WAGTAILIMAGES_IMAGE_MODEL = 'images.CustomImage'\n",
+                "INSTALLED_APPS.append('images')\n",
+            ]:
+                fp.write(line)
 
 
 def makemigrations(app=''):
-    my_cmd(f'./manage.py makemigrations {app}')
+    my_cmd(f'./manage.py makemigrations {app}', show=True)
 
 
 def migrate():
-    my_cmd('./manage.py migrate')
-
-
-def makemigration_copy_images():
-    my_cmd(f'./manage.py makemigrations --empty {APP}')
-    migration_0002 = glob(f'{APP}/migrations/0002_*.py')[0]
-    my_write(migration_0002, get_copy_images_migration())
+    my_cmd('./manage.py migrate', show=True)
 
 
 def start_app_customimage():
-    my_cmd(f'./manage.py startapp {APP}')
+    my_cmd(f'./manage.py startapp {APP}', show=True)
 
 
-def write_customimage_models(complete=True):
-    my_write(f'{APP}/models.py', get_images_models_text({APP}, complete=complete))
+def writefile(filename, contents):
+    with open(filename, 'w') as fp:
+        fp.write(contents)
 
 
-def delete_settings_wagtail_image_model():
-    my_cmd(f'''sed -i -e "/WAGTAILIMAGES_IMAGE_MODEL = 'images.CustomImage'/d" {SETTINGS}''')
+def readfile(filename):
+    with open(filename) as fp:
+        return fp.read()
 
 
-def add_settings_wagtail_image_model():
-    my_cmd(f'''echo "WAGTAILIMAGES_IMAGE_MODEL = 'images.CustomImage'" >> {SETTINGS}''')
+def append_to_file(filename, text):
+    contents = readfile(filename)
+    contents += text
+    writefile(filename, contents)
 
 
-def add_settings_customimages_app():
-    my_cmd(f'''echo "INSTALLED_APPS.append('{APP}')" >> {SETTINGS}''')
+def add_settings_wagtailimages():
+    append_to_file(SETTINGS, "WAGTAILIMAGES_IMAGE_MODEL = 'images.CustomImage'\n")
+
+
+def add_settings_installed_apps():
+    append_to_file(SETTINGS, f"INSTALLED_APPS.append('{APP}')\n")
 
 
 def restore_db(filename=None):
     if not filename:
         filename = ORG_PG_DUMP
-    my_cmd(f'cat {filename} | {PSQL} | tee {POPLOG}')
-    print(f"\n\n\nDone\n\nDo check the file {POPLOG} to see if there where any errors\n\n\n")
+    my_cmd(f'cat {filename} | ./manage.py dbshell | tee {POPLOG}')
 
 
 def dump_db_tmp():
-    my_cmd(f'{DUMP} > {TMP_PG_DUMP}')
+    my_cmd(f'pg_dump {DATABASE} > {TMP_PG_DUMP}')
 
 
-def edit_db_tmp():
-    my_cmd(
-        "sed"
-        r" -e 's/^    \(image_id\) integer\(,\?\)$/    \1 bigint\2/'"
-        r" -e 's/^    \(cover_image_id\) integer\(,\?\)$/    \1 bigint\2/'"
-        r" -e 's/^    \(struct_org_image_id\) integer\(,\?\)$/    \1 bigint\2/'"
-        r" -e 's/^    \(struct_org_logo_id\) integer\(,\?\)$/    \1 bigint\2/'"
-        r" -e 's/^    \(favicon_id\) integer\(,\?\)$/    \1 bigint\2/'"
-        r" -e 's/^    \(logo_id\) integer\(,\?\)$/    \1 bigint\2/'"
-        r" -e 's/^    \(ADD CONSTRAINT coderedcms_.* REFERENCES public\.\)\(wagtailimages_image\)(id)/\1images_customimage(id)/'"
-        f" {TMP_PG_DUMP}"
-        f" > {TMP_PG_RESTORE}"
+def get_edits():
+    return (
+        get_edit_bigint() +
+        get_edit_customimage()
     )
+
+
+def get_edit_bigint():
+    return [(rf'^    ({field}_id) integer(,?)$', r'    \1 bigint\2') for field in IMAGE_FIELDS]
+
+
+def get_edit_customimage():
+    return [
+        (
+            r'^    (ADD CONSTRAINT coderedcms_.*) (REFERENCES public\.)wagtailimages_image\b',
+            r'    \1\n    \2images_customimage'
+        )
+        for field in IMAGE_FIELDS
+    ]
+
+
+def edit_dump(file_in=None, file_out=None):
+    if not file_in:
+        file_in = TMP_PG_DUMP
+    if not file_out:
+        file_out = TMP_PG_RESTORE
+    with open(file_in) as fp_in:
+        with open(file_out, 'w') as fp_out:
+            for line in fp_in:
+                for van, naar in get_edits():
+                    if re.match(van, line):
+                        line = re.sub(van, naar, line)
+                        print(line, end='')
+                        break
+                fp_out.write(line)
 
 
 def restore_db_tmp():
     restore_db(TMP_PG_RESTORE)
 
 
+def delete_app():
+    """delete folder structure"""
+    while APP in os.listdir('.'):
+        for root, folders, files in os.walk('images'):
+            for f in files:
+                f = os.path.join(root, f)
+                os.unlink(f)
+            if not folders:
+                os.rmdir(root)
+
+
 def undo_previous_setup_customimage():
-    rm_mig_0036()            # remove the relevant migrations in virtualenv conderedcms/migrations
-    reset_settings()         # remove the customimage app from INSTALLED_APPS in settings
-    my_cmd('rm -rf images')  # remove the customimage app
+    rm_mig_0036()     # remove the relevant migrations in virtualenv conderedcms/migrations
+    reset_settings()  # remove the customimage app from INSTALLED_APPS in settings
+    delete_app()      # remove the customimage app
 
 
-def check_exist_org_pg_dump():
-    if ORG_PG_DUMP in os.listdir():
+def check_exist_org_pg_dump(db=None):
+    if not db:
+        db = ORG_PG_DUMP
+    if db in os.listdir():
         return
     raise Exception(
         "Oops! You don't seem to have dumped the database:\n"
@@ -221,49 +324,22 @@ def check_exist_org_pg_dump():
     )
 
 
-def reset_db():
-    print(readme)
-    undo_previous_setup_customimage()
-    check_exist_org_pg_dump()
-    drop_create_db()
-    restore_db()
-
-def reset_and_setup_customimage():
+def reset_and_setup():
     reset_db()
-    start_app_customimage()
-    write_customimage_models(complete=False)
-    add_settings_customimages_app()
-    makemigrations(APP)
-    migrate()
-    makemigration_copy_images()
-    migrate()
-    add_settings_wagtail_image_model()
-    write_customimage_models(complete=True)
-    makemigrations()
-    migrate()
-    dump_db_tmp()
-    edit_db_tmp()
-    drop_create_db()
-    restore_db_tmp()
+    setup_customimage()
 
 
 def main():
     if len(sys.argv) == 1:
         print(readme)
     else:
-        reset_and_setup_customimage()
+        reset_and_setup()
 
 
 VIRTUAL_ENV = get_virtualenv()
 CODEREDCMS_MIGRATIONS_FOLDER = get_coderedcms_migrations_folder()
 
 DATABASE       = 'manifestdb'
-USER           = 'chuck'
-POSTGRES       = 'sudo su postgres -c'
-POSTGRES       = 'ssh postgres@localhost'
-PSQL_NODB      = f'{POSTGRES} psql'
-PSQL           = f'{POSTGRES} "psql {DATABASE}"'
-DUMP           = f'{POSTGRES} "pg_dump {DATABASE}"'
 APP            = 'images'
 SETTINGS       = 'manifest/settings/dev.py'
 ORG_PG_DUMP    = 'dump-orig.sql'
@@ -272,6 +348,16 @@ TMP_PG_RESTORE = '_tmp_restore.dmp'
 POPLOG         = '_tmp_populate.log'
 MIG_0035 = sorted(glob(f'{CODEREDCMS_MIGRATIONS_FOLDER}0035_*.py'))
 MIG_0036 = sorted(glob(f'{CODEREDCMS_MIGRATIONS_FOLDER}003[6789]_*.py'))
+IMAGE_FIELDS = [
+    'image',
+    'cover_image',
+    'og_image',
+    'struct_image',
+    'struct_org_image',
+    'struct_org_logo',
+    'favicon',
+    'logo',
+]
 readme = f"""
 Background:
 - Surpisingly, adding an app like this one, automatically adds migrations
@@ -281,6 +367,7 @@ Background:
   pg_dump file and restore the result.
 
 Requirements:
+- the current login user must have access to the database: psql {DATABASE}
 - Stop all programs that access the database {DATABASE}
 - Activate your virtualenv
 - Change directories to the project folder.
@@ -293,19 +380,67 @@ Requirements:
   See for the folder below.
 
 This will reset the project by:
-- dropping the {DATABASE} database and recreate it.
+- cascade dropping all tables in {DATABASE}, leaving it empty.
   (Postgresql will happily preserve the grants to user roles.)
 - use the file <<{ORG_PG_DUMP}>> to repopulate the database.
-- editing <<{SETTINGS}>>
-- remove the complete app folder "./images"
+- editing the settings file: {SETTINGS}
+- remove the complete app folder "./images if it exists"
 - remove these migration files from the codercms folder in your virtualenv:
   {CODEREDCMS_MIGRATIONS_FOLDER}
   {show_mig_0036()}
-You can configure some variables in this file just above the readme variable.
+You can configure some variables in this file just above this readme variable.
 
 To run the setup just supply an arbitrary argument on the command line.
 You should be able to run the setup multiple times.
 """
+
+
+def reset_db():
+    print(readme)
+    my_log(f"check_exist_org_pg_dump({ORG_PG_DUMP})")
+    check_exist_org_pg_dump(ORG_PG_DUMP)
+    my_log("undo_previous_setup_customimage()")
+    undo_previous_setup_customimage()
+    my_log("db_drop_all()")
+    db_drop_all()
+    my_log("restore_db()")
+    restore_db()
+
+
+def setup_customimage():
+    my_log("start_app_customimage()")
+    start_app_customimage()
+    my_log("write_customimage_models(complete=False)")
+    write_customimage_models(complete=False)
+    my_log("add_settings_installed_apps()")
+    add_settings_installed_apps()
+    my_log(f"makemigrations({APP})")
+    makemigrations(APP)
+    my_log("migrate()")
+    migrate()
+    my_log("makemigration_copy_images()")
+    makemigration_copy_images()
+    my_log("migrate()")
+    migrate()
+    my_log("add_settings_wagtailimages()")
+    add_settings_wagtailimages()
+    my_log("write_customimage_models(complete=True)")
+    write_customimage_models(complete=True)
+    my_log("makemigrations()")
+    makemigrations()
+    my_log("migrate()")
+    migrate()
+    my_log("dump_db_tmp()")
+    dump_db_tmp()
+    my_log("edit_dump()")
+    edit_dump()
+    my_log("db_drop_all()")
+    db_drop_all()
+    my_log("restore_db_tmp()")
+    restore_db_tmp()
+    #my_log("setval_custom_app_tables()")
+    #setval_custom_app_tables()
+    print(f"\nDone\nDo check the file {POPLOG} to see if there where any errors\n")
 
 
 if __name__ == '__main__':
